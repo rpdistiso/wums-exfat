ec94546b7e7d01ce8457821734954dd3776c7c07
Blackjay07 <116186005+rpdistiso@users.noreply.github.com>
2025-01-13 15:00:23 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "ExFATModule.hpp"
+
+WUMS_MODULE_NAME("exfat_support");
+WUMS_MODULE_AUTHOR("Blackjay07");
+WUMS_MODULE_VERSION("1.0");
+WUMS_MODULE_LICENSE("GNU3");
+
+extern "C" int WUMS_Load() {
+    return ExFATModule::getInstance().mount() ? 0 : -1;
+}
+
+extern "C" int WUMS_Unload() {
+    return ExFATModule::getInstance().unmount() ? 0 : -1;
+}
